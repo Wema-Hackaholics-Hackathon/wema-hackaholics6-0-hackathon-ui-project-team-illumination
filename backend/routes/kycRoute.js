d763e23030d401045ef05c2beb79042e6ec1fee7
Ayobami <oludemiayobami@gmail.com>
2025-09-10 13:27:54 +0100

    chore: remove other unneeded parameters

--- backend/routes/kycRoute.js
+++ backend/routes/kycRoute.js
@@ -4,19 +4,17 @@ import KYCController from "../controllers/kycController.js";
 const router = express.Router();
 
 router.post("/verifykyc", async (req, res) => {
-  const { fullName, DOB, bvn } = req.body;
+  const { bvn } = req.body;
 
-  if (!fullName || !DOB || !bvn) {
-    res.status(400).json({'error': 'required fields are fullName, DOB and bvn'})
+  if (!bvn) {
+    res.status(400).json({'error': 'bvn is required'})
     return
   }
 
   try {
     const info = await KYCController.verifyBVN(bvn)
-    console.log(info)
     res.json(info)
   } catch (e) {
-    console.log(e.toString())
     res.status(400).json({'error': e})
     return
   }
