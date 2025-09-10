5f76d6f921745b365e3faab999f7b03e29abccb7
Ayobami <oludemiayobami@gmail.com>
2025-09-10 13:15:20 +0100

    feat: add kyc endpoit

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,20 @@
 ﻿import express from "express";
 import cors from "cors";
-require("dotenv").config();
+import dotenv from "dotenv";
+import kycRoute from "./routes/kycRoute.js";
+
+dotenv.config()
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(kycRoute);
 
 // Health check
 app.get("/", (_req, res) => {
   res.json({ ok: true, service: "kyc-trustscore-backend" });
 });
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
