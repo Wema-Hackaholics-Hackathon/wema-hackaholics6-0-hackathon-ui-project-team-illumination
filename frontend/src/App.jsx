389d685d3f6a4b1d7f418770f557d42c7feb552b
Devtaiwo18 <adeyemitaiwo24434@gmail.com>
2025-12-13 13:33:59 +0100

    Fix some error in the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+
 import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom'
 import Header from './components/Layout/Header'
 import Home from './pages/Home'
